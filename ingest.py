236f88c846a441205567228d98cf86f7f73a355d
gomarketingcontentlab-maker <gomarketingcontentlab@gmail.com>
2026-03-06 19:29:05 +0200

    Fix langchain text_splitter import for new version

--- ingest.py
+++ ingest.py
@@ -22,7 +22,7 @@ from langchain_community.document_loaders import (
     UnstructuredMarkdownLoader,
     CSVLoader,
 )
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 
